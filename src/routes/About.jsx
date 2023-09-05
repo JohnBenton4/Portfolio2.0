f0bee2414ff7f159392be7ee4c700174da8ed58a
John Benton <jtbentoniv@gmail.com>
2023-09-05 10:55:02 -0400

    resume button

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import profilePicSmall from "../assets/profilePicSmall.jpg";
+import developerResume from "../assets/developerResume.pdf";
 
 export default function About() {
   return (
@@ -14,18 +15,19 @@ export default function About() {
           <br></br>
           <section className="hello">
             <h3>I'm John. A developer, a designer, and a problem solver.</h3>
-          
+
             <p class="about-me">
-            I'm a developer with a background in music. My dream is to build
-            beautiful websites to help people solve problems. I believe I can
-            have a positive global impact, and I am extremely excited for the
-            opportunities that await me in the near future.
-            <br />
-            If I'm not behind the computer, you can find me spending quality
-            time with my family, building a piece of bespoke furniture, or
-            traveling the world to experience delicious food and breathtaking
-            scenery.
-          </p>
+              I'm a developer with a background in music. My dream is to build
+              beautiful websites to help people solve problems. I believe I can
+              have a positive global impact, and I am extremely excited for the
+              opportunities that await me in the near future.
+              <br />
+              <br />
+              If I'm not behind the computer, you can find me spending quality
+              time with my family, building a piece of bespoke furniture, or
+              traveling the world to experience delicious food and breathtaking
+              scenery.
+            </p>
           </section>
           <section className="quote">
             <p>"This will be a quote of some kind"</p>
@@ -36,17 +38,22 @@ export default function About() {
               <li>- Degrees in Music Education and Percussion Performance</li>
               <li>- 2 years of experience in Web Development</li>
               <li>- 5 years of experience in Finance</li>
-
-
             </ul>
           </section>
           <section className="skills">
             <h3>Skills</h3>
             <ul className="skills-details">
-              <li>Javascript/React.JS/Node.JS/Express.JS/Angular.JS/
-                RestfulAPI/PostgreSQL/Bootstrap/HTML/CSS/Project Management/Education/Music/Woodworker</li>
+              <li>
+                Javascript/React.JS/Node.JS/Express.JS/Angular.JS/
+                RestfulAPI/PostgreSQL/Bootstrap/HTML/CSS/Project
+                Management/Education/Music/Woodworker
+              </li>
             </ul>
           </section>
+
+          <a href={developerResume} target="_blank" rel="noreferrer" className="resume-button">
+          Resume
+          </a>
         </section>
         <section className="about-right">
           <img src={profilePicSmall} alt="profilePicSmall" />
