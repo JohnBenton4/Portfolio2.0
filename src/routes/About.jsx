b7fe97985259519d7ca05416365fd98f4897ee12
John Benton <jtbentoniv@gmail.com>
2023-09-04 20:06:37 -0400

    about page CSS

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,37 +1,39 @@
 import React from "react";
-// import { useNavigate } from "react-router";
+import Sidebar from "../components/sidebar/Sidebar";
 
-const About = () => {
-  // const navigate = useNavigate();
+export default function About() {
   return (
-    <div className="container">
-      {/* <button className="btn" onClick={() => navigate(-1)}>
-        Go Back{" "}
-      </button> */}
-      <section className="about-title">
-        <h1>About</h1>
-      </section>
-      <section className="hello">
-        <h3>I'm John. A developer, a designer, and a problem solver.</h3>
-      </section>
-      <section className="experience">
-        <h4>Experience</h4>
-      </section>
-      <br></br>
-      <ul className="experience-details">
-        <li></li>
-      </ul>
-      <section className="skills">
-        <h4>Skills</h4>
-      </section>
-      <br></br>
-      <ul className="skills-details">
-        <li></li>
-      </ul>
-    </div>
+    <>
+      <Sidebar />
+      <div className="container-about">
+        <section className="about-left">
+          <div className="about-title">
+            <h1>About</h1>
+          </div>
+          <br></br>
+          <section className="hello">
+            <h3>I'm John. A developer, a designer, and a problem solver.</h3>
+          </section>
+          <section className="quote">
+            <p>This will be a quote of some kind</p>
+          </section>
+          <section className="experience">
+            <h3>Experience</h3>
+            <ul className="experience-details">
+              <li>This is a list item</li>
+            </ul>
+          </section>
+          <section className="skills">
+            <h3>Skills</h3>
+            <ul className="skills-details">
+              <li>Here are some skills</li>
+            </ul>
+          </section>
+        </section>
+        <section className="about-right">
+          This is a photo.
+        </section>
+      </div>
+    </>
   );
-};
-
-
-
-export default About;
+}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,43 +1,32 @@
 import { Link } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
 
-
 export default function Home() {
-
-// const Home = () => {
   return (
     <>
-    <Sidebar />
-    <div className="container">
+      <Sidebar />
+      <div className="container-home">
         <div className="intro">
-            <div className="name">
-            JOHN BENTON
-            </div>
-            <div className="credentials">
-                Web Developer / Carpenter
-            </div>
+          <div className="name">JOHN BENTON</div>
+          <div className="credentials">Web Developer / Carpenter</div>
         </div>
-      <div className="link-container">
-        <div className="links">
-          <Link to="/work">
-            <h1>Work</h1>
-          </Link>
-          <Link to="/about">
-            <h1>About</h1>
-          </Link>
-          <Link to="/about">
-            <h1>Resume</h1>
-          </Link>
-          <Link to="/about">
-            <h1>Contact</h1>
-          </Link>
+        <div className="link-container">
+          <div className="links">
+            <Link to="/work">
+              <h1>Work</h1>
+            </Link>
+            <Link to="/about">
+              <h1>About</h1>
+            </Link>
+            <Link to="/about">
+              <h1>Resume</h1>
+            </Link>
+            <Link to="/about">
+              <h1>Contact</h1>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
-};
-
-// export default Home;
-
-
+}
